8d17b3276511c8dc4a337e58dfaa63cc916e7f58
“Richard Middleton” <Richard@richardmiddleton.me>
2017-09-04 20:08:39 +0100

    set up options for yarg commands and refactored

--- nodejs course/notes-node/app.js
+++ nodejs course/notes-node/app.js
@@ -5,14 +5,38 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const argv = yargs.argv;
+var titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+}
+
+var bodyOptions = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+}
+
+const argv = yargs.command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+})
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 
 var command = argv._[0];
 
 
 if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
-    if(note !== null && typeof note === 'object') {
+    if (note !== null && typeof note === 'object') {
         notes.logNote(note);
     }
     else {
@@ -29,8 +53,8 @@ else if (command === 'list') {
 
 else if (command === 'read') {
     var note = notes.getNote(argv.title);
-    
-    if(note !== null && typeof note === 'object') {
+
+    if (note !== null && typeof note === 'object') {
         notes.logNote(note);
     }
     else {
@@ -47,6 +71,6 @@ else if (command === 'remove') {
     console.log('---------');
 }
 
-else { 
+else {
     console.log('Command not recognised');
 }
