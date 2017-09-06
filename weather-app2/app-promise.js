6dd627269b90438ed1b060f4b372fb86c04db272
“Richard Middleton” <Richard@richardmiddleton.me>
2017-09-06 23:49:33 +0100

    added formatting for console.log

--- weather-app2/app-promise.js
+++ weather-app2/app-promise.js
@@ -18,6 +18,8 @@ const argv = yargs
 
 var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+var space = " ";
+var line = "============================";
 
 axios.get(geocodeUrl).then((response) => {
     if (response.data.status === 'ZERO_RESULTS') {
@@ -27,14 +29,18 @@ axios.get(geocodeUrl).then((response) => {
     var weather = response.data.results[0].geometry.location;
 
     var weatherUrl = `https://api.darksky.net/forecast/99f9e746e67c2662d83a7e4e2976dd04/${weather.lat},${weather.lng}?units=uk2`;
-
+    console.log(space);
+    console.log(line);
     console.log(response.data.results[0].formatted_address);
+    console.log(line);
 
     return axios.get(weatherUrl);
 }).then((response) => {
     var temperature = response.data.currently.temperature;
     var apparentTemperature = response.data.currently.apparentTemperature;
     console.log(`It's currently ${temperature}, but it feels like ${apparentTemperature}.`);
+    console.log(line);
+    console.log(space);
 }).catch((e) => {
     if (e.code === 'ENOTFOUND') {
         console.log('unable to connect to API Server');
